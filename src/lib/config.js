a029beb5ac07ee839fe639dfdb065f5376da5150
IgihozoColombe <77879746+IgihozoColombe@users.noreply.github.com>
2026-03-31 14:05:52 +0200

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000',
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://ikiguzi-app-backend.onrender.com',
 }
 
